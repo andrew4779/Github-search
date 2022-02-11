4aedd6ab8fed3eb5bcea5215108ffaa48f0206d8
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-02-11 21:49:25 +0300

    made new updates

--- src/app/repo-search/repo-search.component.ts
+++ src/app/repo-search/repo-search.component.ts
@@ -1,4 +1,23 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-repo-search',
+//   templateUrl: './repo-search.component.html',
+//   styleUrls: ['./repo-search.component.css']
+// })
+// export class RepoSearchComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Repository } from '../repository';
+import { User } from '../user';
+import { UserRequestService } from '../user-http/user-request.service';
 
 @Component({
   selector: 'app-repo-search',
@@ -6,10 +25,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo-search.component.css']
 })
 export class RepoSearchComponent implements OnInit {
+  user!: User
+  repos!: Repository
 
-  constructor() { }
+  constructor(private http: HttpClient, private userService: UserRequestService) {
+    
+  }
 
   ngOnInit(): void {
+    this.userService.userRequest(this.user.username)
+    this.user = this.userService.user
+    
   }
 
 }
