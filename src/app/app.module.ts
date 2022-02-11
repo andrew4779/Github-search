22fe1cbdf5a3a61f0bac2fa41f99e3981b6928a0
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-02-11 22:37:31 +0300

    modified file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RepoSearchComponent } from './repo-search/repo-search.component';
 import { UserFormComponent } from './user-form/user-form.component';
-import { UserHttpComponent } from './user-http/user-http.component';
-import { UserServiceComponent } from './user-service/user-service.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RepoSearchComponent,
-    UserFormComponent,
-    UserHttpComponent,
-    UserServiceComponent
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
