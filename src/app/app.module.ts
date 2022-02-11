dea982d7672fa3e98bd967267bad16e2e546eaab
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-02-11 16:33:34 +0300

    added new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RepoSearchComponent } from './repo-search/repo-search.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RepoSearchComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
